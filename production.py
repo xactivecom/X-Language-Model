017e8247806f289cd743ca23242083e1071600ba
xactivecom <stuart@xactive.com>
2026-04-21 08:51:55 -0400

    GTP-2 model training

--- production.py
+++ production.py
@@ -1,5 +1,52 @@
 import torch
 import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+
+import tiktoken
+
+
+# Class to load dataset end GPT-2 encode
+class GPTDatasetV1(Dataset):
+  def __init__(self, txt, tokenizer, max_length, stride):
+    self.input_ids = []
+    self.target_ids = []
+
+    # Tokenize the entire text
+    token_ids = tokenizer.encode(txt, allowed_special={ "<|endoftext|>" })
+    assert len(token_ids) > max_length, "Number of tokenized inputs must at least be equal to max_length+1"
+
+    # Use a sliding window to chunk the book into overlapping sequences of max_length
+    for i in range(0, len(token_ids) - max_length, stride):
+      input_chunk = token_ids[i:i + max_length]
+      target_chunk = token_ids[i + 1: i + max_length + 1]
+      self.input_ids.append(torch.tensor(input_chunk))
+      self.target_ids.append(torch.tensor(target_chunk))
+
+  def __len__(self):
+    return len(self.input_ids)
+
+  def __getitem__(self, idx):
+    return self.input_ids[idx], self.target_ids[idx]
+
+
+# Function to create encoding using PyTorch dataloader.
+# Dataloader provides utilities for input data batching and shuffling.
+def create_dataloader_v1(text, batch_size=4, max_length=256, stride=128, shuffle=True, drop_last=True, num_workers=0):
+
+  # Initialize the tokenizer
+  tokenizer = tiktoken.get_encoding("gpt2")
+
+  # Create dataset
+  dataset = GPTDatasetV1(text, tokenizer, max_length, stride)
+
+  # Create iterable dataloader to load batches of data:
+  # - batch_size loads batch of data of specified size during each iteration
+  # - shuffle=True changes the order of data indices to achieve better generalization
+  # - drop_last=True drops the last batch if it is shorter than the batch_size to prevent loss spikes during training
+  # - num_workers does not work in notebooks
+  dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last, num_workers=num_workers)
+  return dataloader
+
 
 # Multi-head causal attention mechanism using parallelism from matrix multiplication.
 class MultiHeadAttention(nn.Module):
@@ -135,3 +182,110 @@ class FeedForward(nn.Module):
   def forward(self, x):
     return self.layers(x)
 
+
+class TransformerBlock(nn.Module):
+  def __init__(self, cfg):
+    super().__init__()
+
+    # Declare network layers
+    self.att = MultiHeadAttention(
+      d_in=cfg["emb_dim"],
+      d_out=cfg["emb_dim"],
+      context_length=cfg["context_length"],
+      num_heads=cfg["n_heads"], 
+      dropout=cfg["drop_rate"],
+      qkv_bias=cfg["qkv_bias"])
+    self.ff = FeedForward(cfg)
+    self.norm1 = LayerNorm(cfg["emb_dim"])
+    self.norm2 = LayerNorm(cfg["emb_dim"])
+    self.drop_shortcut = nn.Dropout(cfg["drop_rate"])
+
+  def forward(self, x):
+    # Shortcut connection for attention block
+    shortcut = x
+    x = self.norm1(x)
+    x = self.att(x)  # Shape [batch_size, num_tokens, emb_size]
+    x = self.drop_shortcut(x)
+    x = x + shortcut  # Add the original input back
+
+    # Shortcut connection for feed forward block
+    shortcut = x
+    x = self.norm2(x)
+    x = self.ff(x)
+    x = self.drop_shortcut(x)
+    x = x + shortcut  # Add the original input back
+
+    return x
+
+
+class GPTModel(nn.Module):
+  def __init__(self, cfg):
+    super().__init__()
+
+    # Declare input and output embedding layers
+    self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
+    self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
+
+    # Declare dropout layer
+    self.drop_emb = nn.Dropout(cfg["drop_rate"])
+
+    # Declare transformer block layers
+    self.xform_blocks = nn.Sequential(
+      *[TransformerBlock(cfg) for _ in range(cfg["n_layers"])])
+
+    # Declare final normalization layer
+    self.final_norm = LayerNorm(cfg["emb_dim"])
+
+    # Declare linear layer
+    self.out_head = nn.Linear(cfg["emb_dim"], cfg["vocab_size"], bias=False)
+
+  def forward(self, in_idx):
+    batch_size, seq_len = in_idx.shape
+
+    # Map vocabulary space to embedding space
+    tok_embeds = self.tok_emb(in_idx)
+    pos_embeds = self.pos_emb(torch.arange(seq_len, device=in_idx.device))
+
+    # Combine token and positional embeddings
+    # Shape [batch_size, num_tokens, emb_size]
+    x = tok_embeds + pos_embeds
+
+    # Process dropout
+    x = self.drop_emb(x)
+
+    # Process transformer blocks
+    x = self.xform_blocks(x)
+
+    # Process final normalization
+    x = self.final_norm(x)
+
+    # Map embedding space to vocabulary space
+    logits = self.out_head(x)
+    return logits
+
+
+# Generate text one token at a time
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+  # Truncate current context if it exceeds the supported context size
+  for _ in range(max_new_tokens):
+    # Get token context
+    idx_cond = idx[:, -context_size:]
+
+    # Get the predictions from the tokens.
+    # Disable gradient computation to suppress the model training.
+    with torch.no_grad():
+      logits = model(idx_cond)
+
+    # Focus only on the last time step
+    logits = logits[:, -1, :]
+
+    # Apply softmax to get probabilities
+    probas = torch.softmax(logits, dim=-1)
+
+    # Get the idx of the vocab entry with the highest probability value
+    idx_next = torch.argmax(probas, dim=-1, keepdim=True)
+
+    # Append sampled index to the running sequence
+    idx = torch.cat((idx, idx_next), dim=1)
+
+  return idx
